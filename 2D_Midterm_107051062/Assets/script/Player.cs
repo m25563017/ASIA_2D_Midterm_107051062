9cf74975a951411fee2bc9ddef0f4fa15d0bd4fe
m25563017 <71679886+m25563017@users.noreply.github.com>
2020-11-15 03:39:01 +0800

    程式

--- 2D_Midterm_107051062/Assets/script/Player.cs
+++ 2D_Midterm_107051062/Assets/script/Player.cs
@@ -10,21 +10,18 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.name == "傳送門")
+        if (collision.name == "Door")
         {
+            print("123");
             final.SetActive(true);  
         }
 
-        // 如果碰到物件的標籤式櫻桃就吃掉她
-        if (collision.tag == "櫻桃")
-        {
-            // 刪除(碰到的遊戲物件)
-            // gameObject 此腳本的遊戲物件
-            Destroy(collision.gameObject);
-            // 遞增
+        
+        if (collision.tag == "橡果")
+        {          
+            Destroy(collision.gameObject);           
             count++;
-            // 道具數量的文字 = "櫻桃數量：" + 數量
-            textCount.text = "櫻桃數量：" + count;
+            textCount.text = "Count：" + count;
         }
     }
 }
